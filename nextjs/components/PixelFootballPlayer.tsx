99774019e40b79a7d29037844cbd3cbe7299bf86
Jan Mikeš <j.mikes@me.com>
2025-12-03 16:15:18 +0100

    Player

--- nextjs/components/PixelFootballPlayer.tsx
+++ nextjs/components/PixelFootballPlayer.tsx
@@ -2,15 +2,11 @@
 
 export default function PixelFootballPlayer() {
   return (
-    <div className="pixel-runner-container">
-      {/* Animated wrapper that moves across screen */}
-      <div className="pixel-player-wrapper">
-        {/* Frame 1 - legs apart */}
-        <div className="pixel-frame-1"></div>
-        {/* Frame 2 - legs together */}
-        <div className="pixel-frame-2"></div>
-        {/* Ball at feet */}
-        <div className="pixel-ball"></div>
+    <div className="runner-container">
+      {/* Animated sprite that runs across screen */}
+      <div className="sprite-wrapper">
+        <div className="sprite-character"></div>
+        <div className="sprite-ball"></div>
       </div>
     </div>
   );
